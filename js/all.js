d7f119ec872afe54da7b396673fb3e63afead309
Freddie <lin.pengyu@outlook.com>
2022-10-05 22:33:03 +0800

    修改部分內容並新增未完成的hover effect

--- js/all.js
+++ js/all.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $('.fa-bars').click(function (e) { 
-        e.preventDefault();
-        $('.header ul').toggleClass('show');
-    });
+  $(".fa-bars").click(function (e) {
+    e.preventDefault();
+    $(".header ul").toggleClass("show");
+  });
 });
